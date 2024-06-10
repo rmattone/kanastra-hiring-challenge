352db0136a6be1e2403264e7afc8877d188647b7
rafaelm <rafaelfmattone@gmail.com>
2024-06-10 00:21:09 -0300

    Start docker implementation

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   publicPath: process.env.NODE_ENV === 'production' ? process.env.BASE_URL : '/',
+  server: {
+    hmr: {
+      host: "0.0.0.0",
+    },
+    port: 3000,
+    host: true,
+  },
   plugins: [
     vue(),
+    laravel({
+      input: ["resources/css/app.css", "resources/js/app.js"],
+      refresh: true,
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
